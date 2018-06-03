c840fd9a0ea195d0eeee2dedcac0014c8f523f4b
Gabriele Canepa <gabriele@canepa.io>
2018-06-03 22:22:35 +0100

    Style index and add show action

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,20 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show]
+
   def index
     @flats = Flat.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :image)
   end
 end
