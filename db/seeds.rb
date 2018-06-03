846d3c3db0c09cdc9c4a6aa358c8b6308706d068
Gabriele Canepa <gabriele@canepa.io>
2018-06-03 18:17:32 +0100

    Add flat model, flats controller and CRUD routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+10.times do
+  Flat.create!([
+    {
+      name:               Faker::Company.catch_phrase,
+      address:            "#{Faker::Address.street_address}, #{Faker::Address.street_address}",
+      description:        "#{Faker::Company.catch_phrase}, #{Faker::Company.bs}.",
+      price_per_night:    rand(100) + 20,
+      number_of_guests:   rand(8) + 1
+    }
+  ])
+end
+
+puts 'Finished!'
